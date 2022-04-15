a94e8f6d37e8768e44985611e582f0bb34941faa
HanaaAlzahabi <hanaa.al.zahabi@hotmail.com>
2022-04-15 11:32:03 +0200

    homepage/getCategories

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import dotenv from "dotenv"
 import express from "express"
-import mysql from "mysql2"
+import mysql from "mysql2/promise"
 import signupRoute from "./routes/signup/route.js"
 import loginRoute from "./routes/login/route.js"
-import {eventRoute} from "./routes/homepage/route.js"
-import {getUserInfoRoute} from "./routes/homepage/route.js"
-
+import {eventRoute, getUserInfoRoute, getCategoriesRoute} from "./routes/homepage/route.js"
 //Env config
 dotenv.config();
 //Server config
@@ -28,6 +26,7 @@ app.post("/signup", (req, res) => signupRoute(connection, req, res));
 app.post("/login", (req, res) => loginRoute(connection, req, res));
 app.post("/getUserInfo", (req, res) => getUserInfoRoute(connection, req, res))
 app.get("/event", (req, res) => eventRoute(connection, req, res))
+app.get("/getCategories", (req, res) => getCategoriesRoute(connection, req, res))
 
 
 app.listen(process.env.PORT || 3000, () => {
