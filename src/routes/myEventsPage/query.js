75dfc4a86d1a952906b28e98cf1cc9908052e995
mguillevic <marie.guillevic@insa-lyon.fr>
2022-04-20 09:52:34 +0200

    Update query.js

--- src/routes/myEventsPage/query.js
+++ src/routes/myEventsPage/query.js
@@ -1,5 +1,5 @@
 const comingEventsQuery = () => {
-    const req = `SELECT event.name, event.photo as ImageEvent, event.date_timestamp, event.place, creator.photo as ImageProfil  FROM eve.Event as event, eve.User as creator, eve.Participation as p  where event.date_timestamp>=Now() and creator.id=event.creator_id and event.id=p.event_id and p.user_id=?`
+    const req = `SELECT event.name, event.photo as ImageEvent, event.date_timestamp, event.place, creator.photo as ImageProfil  FROM eve.Event as event, eve.User as creator, eve.Participation as p  where event.date_timestamp>=Now() and creator.id=event.creator_id and event.id=p.event_id and p.user_id=? order by event.date_timestamp`
     return req
 };
   
