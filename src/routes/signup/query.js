0c55e750041d6e3e3a58bf98a42fd16945b41364
mwmouawad <mwmouawad@gmail.com>
2022-05-02 10:41:44 +0200

    added verified

--- src/routes/signup/query.js
+++ src/routes/signup/query.js
@@ -1,7 +1,7 @@
 const signupQuery = () => {
   //Checker formatation
-  const req = `INSERT INTO eve.User (name, surname, mail, phone, city, street, street_number, region, zip_code, address_complement, gender, date_birth, user_password, description, admin, school_id, photo)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, FALSE, ?, '')`;
+  const req = `INSERT INTO eve.User (verified, name, surname, mail, phone, city, street, street_number, region, zip_code, address_complement, gender, date_birth, user_password, description, admin, school_id, photo)
+        VALUES (FALSE, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, FALSE, ?, '')`;
   return req;
 };
 
